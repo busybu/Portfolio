1ac31de268555bb455e45dc769bdf4e805e49d83
busybu <gabrielle.fialkoski@hotmail.com>
2022-12-18 20:37:49 -0300

    add: initial page, history page

---
+++ script.js
@@ -0,0 +1,25 @@
+const targetDiv = document.getElementById("myself");
+const btn = document.getElementById('show-myself');
+
+$(btn).click(function () {
+    $(btn).hide(1000);
+    $(targetDiv).show(1000);
+});
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
